7c30b479b48fa9d22725cd3c7747144f5d263d54
David Jadlowkier <davidjadlowkier@Caroline-sin-MacBook-Pro.local>
2025-03-05 12:32:26 +0100

    add firebase config content

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoJ_7LfOZScGCqJ0BfEAu8Q_j0mBKWVfg",
+  authDomain: "eiv-reza-bakery.firebaseapp.com",
+  projectId: "eiv-reza-bakery",
+  storageBucket: "eiv-reza-bakery.firebasestorage.app",
+  messagingSenderId: "396052438316",
+  appId: "1:396052438316:web:146ccd71ad0c6b0ebba089",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
